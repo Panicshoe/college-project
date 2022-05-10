b13c4c29520f7d6e5835c1e3d6679c674bdbc46a
Panicshoe <86228058+Panicshoe@users.noreply.github.com>
2022-05-10 15:50:17 +0530

    Initial Commit
    .gitignore
    .gitatributes

---
+++ endless runne/Assets/scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Text highscoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscoreText.text = "Highscore : " + ((int) PlayerPrefs.GetFloat (" Highscore ")).ToString();
+    }
+
+    // Update is called once per frame
+  
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
